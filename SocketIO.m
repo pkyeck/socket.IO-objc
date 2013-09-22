82459870e6f00fbe666a88ee279ee0c553345d95
Sergei <s.cherepanov@gmail.com>
2013-09-22 13:12:04 -0700

    Fix improper use of NSLocalizedDescriptionKey.
    This commit fixes a typo where an underlying error (NSError object) was associated with NSLocalizedDescriptionKey that is expected to be NSString.

--- SocketIO.m
+++ SocketIO.m
@@ -676,7 +676,7 @@ NSString* const SocketIOException = @"SocketIOException";
     _isConnecting = NO;
     
     if ([_delegate respondsToSelector:@selector(socketIO:onError:)]) {
-        NSMutableDictionary *errorInfo = [NSDictionary dictionaryWithObject:error forKey:NSLocalizedDescriptionKey];
+        NSMutableDictionary *errorInfo = [NSDictionary dictionaryWithObject:error forKey:NSUnderlyingErrorKey];
         
         NSError *err = [NSError errorWithDomain:SocketIOError
                                            code:SocketIOHandshakeFailed
